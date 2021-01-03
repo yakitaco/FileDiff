961ee844b40f01868fbb3d1efa08e47ad24bbe25
yaki_taco <yaki_taco@yahoo.co.jp>
2021-01-03 22:23:16 +0900

    Add directory selection dialog.

--- FileDiff/FileDiff/Form1.Designer.cs
+++ FileDiff/FileDiff/Form1.Designer.cs
@@ -38,6 +38,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Diff 1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -47,6 +48,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "Diff 2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox1
             // 
--- FileDiff/FileDiff/Form1.cs
+++ FileDiff/FileDiff/Form1.cs
@@ -13,5 +13,53 @@ namespace FileDiff {
         public Form1() {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e) {
+            //FolderBrowserDialogクラスのインスタンスを作成
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            //上部に表示する説明テキストを指定する
+            fbd.Description = "フォルダを指定してください。";
+            //ルートフォルダを指定する
+            //デフォルトでDesktop
+            fbd.RootFolder = Environment.SpecialFolder.Desktop;
+            //最初に選択するフォルダを指定する
+            //RootFolder以下にあるフォルダである必要がある
+            fbd.SelectedPath = @"C:\Windows";
+            //ユーザーが新しいフォルダを作成できるようにする
+            //デフォルトでTrue
+            fbd.ShowNewFolderButton = true;
+
+            //ダイアログを表示する
+            if (fbd.ShowDialog(this) == DialogResult.OK) {
+                //選択されたフォルダを表示する
+                Console.WriteLine(fbd.SelectedPath);
+                textBox1.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            //FolderBrowserDialogクラスのインスタンスを作成
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            //上部に表示する説明テキストを指定する
+            fbd.Description = "フォルダを指定してください。";
+            //ルートフォルダを指定する
+            //デフォルトでDesktop
+            fbd.RootFolder = Environment.SpecialFolder.Desktop;
+            //最初に選択するフォルダを指定する
+            //RootFolder以下にあるフォルダである必要がある
+            fbd.SelectedPath = @"C:\Windows";
+            //ユーザーが新しいフォルダを作成できるようにする
+            //デフォルトでTrue
+            fbd.ShowNewFolderButton = true;
+
+            //ダイアログを表示する
+            if (fbd.ShowDialog(this) == DialogResult.OK) {
+                //選択されたフォルダを表示する
+                Console.WriteLine(fbd.SelectedPath);
+                textBox2.Text = fbd.SelectedPath;
+            }
+        }
     }
 }
